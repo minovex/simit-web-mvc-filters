f53ca757b6aac61b91582302c0c63eb3fb26234c
ozan@minovex.com <mx7vqabc>
2015-03-12 14:34:21 +0200

    Initial commit

---
+++ Simit.Web.Mvc.Filters/Simit.Web.Mvc.Filters/CompressAttribute.cs
@@ -0,0 +1,39 @@
+﻿namespace Simit.Web.Mvc.Filters
+{
+    using System.IO.Compression;
+    using System.Web;
+    using System.Web.Mvc;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class CompressAttribute : ActionFilterAttribute
+    {
+        /// <summary>
+        /// Called by the ASP.NET MVC framework before the action method executes.
+        /// </summary>
+        /// <param name="filterContext">The filter context.</param>
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var encodingsAccepted = filterContext.HttpContext.Request.Headers["Accept-Encoding"];
+
+            if (string.IsNullOrEmpty(encodingsAccepted)) return;
+
+            HttpResponseBase response = filterContext.HttpContext.Response;
+            if (response.Filter == null) return;
+
+            encodingsAccepted = encodingsAccepted.ToLowerInvariant();
+
+            if (encodingsAccepted.Contains("gzip"))
+            {
+                response.AppendHeader("Content-encoding", "gzip");
+                response.Filter = new GZipStream(response.Filter, CompressionMode.Compress);
+            }
+            else if (encodingsAccepted.Contains("deflate"))
+            {
+                response.AppendHeader("Content-encoding", "deflate");
+                response.Filter = new DeflateStream(response.Filter, CompressionMode.Compress);
+            }
+        }
+    }
+}
